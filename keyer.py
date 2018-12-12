2667d2b83b3f7591d39c10b0ca43467c946f1b08
amazingproducer <ophthalmencephalon@gmail.com>
2018-12-12 09:12:57 +0000

    Add support for devices that require inverted keying signals

--- keyer.py
+++ keyer.py
@@ -52,18 +52,17 @@ def wordgap():
   time.sleep(wordspace)
 
 def dah():
-  cw_key(False)
-  cw_key(True)
+  cw_key(key_release)
+  cw_key(key_press)
   time.sleep(dahspeed)
-  cw_key(False)
+  cw_key(key_release)
   time.sleep(ditspeed)
-
 
 def dit():
-  cw_key(False)
-  cw_key(True)
+  cw_key(key_release)
+  cw_key(key_press)
   time.sleep(ditspeed)
-  cw_key(False)
+  cw_key(key_release)
   time.sleep(ditspeed)
 
 def word(w):
@@ -94,15 +93,22 @@ parser = argparse.ArgumentParser(description='CW keyer for serial interface.')
 parser.add_argument('-d', '--device', dest='device', default='/dev/ttyUSB0', help='Path to serial device. (default: /dev/ttyUSB0)')
 parser.add_argument('-w', '--wpm', dest='wpm', type=int, default=20, help='CW keying speed in words per minutes. (default: 20 wpm)')
 parser.add_argument('-t', '--text', dest='text', required=True, help='Text to transmit. Surround multiple words by quotes.')
-parser.add_argument('--dtr', dest='dtr', action='store_true', help='Use DTR pin instead of RTS pin for keying')
+parser.add_argument('--dtr', dest='dtr', action='store_true', help='Use DTR pin instead of RTS pin for keying.')
+parser.add_argument('--invert', dest='invert', action='store_true', help='Invert logic signals on pin used for keying.')
 args = parser.parse_args()
 
 ser = serial.Serial(args.device, 9600)
 if args.dtr:
-   cw_key = ser.setDTR
+  cw_key = ser.setDTR
 else:
-   cw_key = ser.setRTS
+  cw_key = ser.setRTS
 
+if args.invert:
+  key_press = False
+  key_release = True
+else:
+  key_press = True
+  key_release = False
 
 ditspeed = (1200.0 / float(args.wpm)) / 1000.0
 dahspeed = ditspeed * 3
