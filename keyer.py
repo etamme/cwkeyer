c3df61b64467d5212a343bcb28432720f1d4c538
amazingproducer <ophthalmencephalon@gmail.com>
2019-02-08 21:21:31 +0000

    WIP encode prosigns as single character

--- keyer.py
+++ keyer.py
@@ -51,6 +51,30 @@ cut_nums = {
             '0':'_'
            }
 
+# These are the default macro keys/CW prosigns in fldigi
+#fldigi_default_macros = {
+#  '=':'<BT>',
+#  '~':'<AA>',
+#  '>':'<AR>',
+#  '<':'<AS>',
+#  '{':'<HM>',
+#  '&':'<INT>',
+#  '%':'<SK>',
+#  '+':'<KN>',
+#  '}':'<VE>' } 
+
+#fldigi_prosigns = {
+#  '=':'_..._',
+#  '~':'._._',
+#  '>':'._._.',
+#  '<':'._...',
+#  '{':'....__',
+#  '&':'.._._',
+#  '%':'..._._',
+#  '+':'_.__.',
+#  '}':'..._.' }
+  
+
 class cw_text_parser:
   def __init__(self, wpm):
     # FIXME: Check if we really need float() here
@@ -89,6 +113,7 @@ class cw_text_parser:
       c=w[i]
       # catch a new macro escape
       if c == "<":
+        code=""
         # setup the first char of the macro past the macro escape char '<'
         i+=1
         macro=w[i]
@@ -99,54 +124,69 @@ class cw_text_parser:
           if macro == "+" or macro == "-":
             if macro == "+":
               self.wpm += 5
+              if args.debugmacro:
+                print("<SPEED INCREASE>")
             else:
               self.wpm += -5
+              if args.debugmacro:
+                print("<SPEED DECREASE>")
+                #TODO index speed macros if they are used with prosigns
+                #     so that the macros are displayed in chronological
+                #     order.
             self.recalculate_speeds()
             #reset current macro command
             macro = ""
-          elif macro=="BT":
-            self.key(self.dahspeed)
-            self.wait_intracharspace()
-            self.key(self.ditspeed)
-            self.wait_intracharspace()
-            self.key(self.ditspeed)
-            self.wait_intracharspace()
-            self.key(self.ditspeed)
-            self.wait_intracharspace()
-            self.key(self.dahspeed)
+          elif macro not in ("+","-",">","<"):
+            code = code + chars[macro]
             macro=""
           elif macro == ">":
+            self.send_char(code, i, w)
             #break out of the while loop
             if i < len(w)-1:
               i+=1
               c=w[i]
+              macro=""
+              code=""
             break
+          
           i+=1
           macro = macro + str(w[i])
       else:
         try:
           code = chars[c]
+          self.send_char(code, i, w)
           i+=1
+          
         except KeyError:
           # skip unknown chars
           i+=1
           continue
 
-        codeidx=0
-        while codeidx < len(code):
-          dahdit=code[codeidx]
-          if dahdit == '_':
-            self.key(self.dahspeed)
-          else:
-            self.key(self.ditspeed)
-          # include intRA char spacing unless this is the last dah/dit
-          if codeidx < len(code)-1:
-            self.wait_intracharspace()
-          codeidx+=1
-        # include intER char spacing unless this is the last char in the word
-        if i < len(w):
-          self.wait_charspace()
-    self.wait_wordspace()
+  def send_char(self, code, word_index, parsed_word):
+    if args.debugmacro:
+      if parsed_word[word_index] == ">":
+        for x in "+-":
+           parsed_word = parsed_word.replace(x,"")
+        if parsed_word != "<>":
+          print(parsed_word + ":" + code)
+      else:  
+        print(parsed_word[word_index] + ":" + code)
+    codeidx=0
+    while codeidx < len(code):
+      dahdit=code[codeidx]
+      if dahdit == '_':
+        self.key(self.dahspeed)
+      else:
+        self.key(self.ditspeed)
+      # include intRA char spacing unless this is the last dah/dit
+      if codeidx < len(code)-1:
+        self.wait_intracharspace()
+      codeidx+=1
+    # include intER char spacing unless this is the last char in the word
+    if word_index < len(parsed_word):
+      self.wait_charspace()
+    else:
+      self.wait_wordspace()
 
 # this is for diagnostic purposes only
 def paris():
@@ -163,6 +203,7 @@ parser.add_argument('-t', '--text', dest='text', required=True, help='Text to tr
 parser.add_argument('--dtr', dest='dtr', action='store_true', help='Use DTR pin instead of RTS pin for keying.')
 parser.add_argument('--invert', dest='invert', action='store_true', help='Invert logic signals on pin used for keying.')
 parser.add_argument('--cut-nums',dest='cutnums',action='store_true',help='Use contest-style abbreviated numbers.')
+parser.add_argument('--debug-macro',dest='debugmacro',action='store_true',help='Show Prosign and character code patterns as they are sent.')
 args = parser.parse_args()
 
 ser = serial.Serial(args.device, 9600)
@@ -179,7 +220,7 @@ else:
   key_open = False
 
 if args.cutnums:
-  chars.update(cut_nums)
+  chars.update(cut_nums)  
 
 # iterate over the passed string as individual words
 p = cw_text_parser(args.wpm)
